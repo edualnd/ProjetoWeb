db7917290617f36908059cf184b88d8ea20b78c9
Eduarda-Ubuntu <ifeduarda.luiz@gmail.com>
2025-05-22 18:56:37 -0300

    schema de validação

---
+++ backend/src/schemas/userSchema.js
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const userSchema = z.object({
+  username: z.string().min(3).max(30),
+  email: z.string().min(3).max(30).email(),
+  password: z.string().min(3).max(20),
+  role: z.enum(['COMMON', 'PROFESSIONAL']).default('COMMON').optional(),
+  userImage: z.string().optional(),
+  bio: z.string().min(0).max(100).optional(),
+});
+
+
+export default userSchema
