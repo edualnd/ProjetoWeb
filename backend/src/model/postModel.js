ac4b598bb7dfeb44175d285e52a4694e6d94e474
kevin4bmael <kevin4bmael@gmail.com>
2025-06-03 14:39:26 -0300

    add get, update and delete models also initial stage of post validator

--- backend/src/model/postModel.js
+++ backend/src/model/postModel.js
@@ -1,11 +1,45 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import postSchema from '../schemas/postSchema.js';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export async function create(post){
-    const result = await prisma.publication.create({
-        data: post
-    })
+export const postValidator = (post, partial = null) => {
+  if (partial) {
+    return postSchema.partial(partial).safeParse(post);
+  }
+  return postSchema.safeParse(post);
+};
 
-    return result
+export async function create(post) {
+  const result = await prisma.publication.create({
+    data: post,
+  });
+
+  return result;
+}
+
+export async function update(authorId, publicationId, post) {
+  const result = await prisma.publication.update({
+    where: {
+      publicationId: Number(publicationId),
+      authorId: authorId,
+    },
+    data: post,
+  });
+  return result;
+}
+
+export async function deletePost(authorId, publicationId) {
+  const result = await prisma.publication.delete({
+    where: {
+      publicationId: Number(publicationId),
+      authorId: authorId,
+    },
+  });
+  return result;
+}
+
+export async function getList() {
+  const result = await prisma.publication.findMany();
+  return result;
 }
