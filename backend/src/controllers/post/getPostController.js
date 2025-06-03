28b44db86d42c5b8a1303e9d68fb476b4c5dad33
kevin4bmael <kevin4bmael@gmail.com>
2025-06-03 14:42:04 -0300

    get controller from get post router

---
+++ backend/src/controllers/post/getPostController.js
@@ -0,0 +1,11 @@
+import { getList } from '../../model/postModel.js';
+
+export default async function getPostController(req, res) {
+  try {
+    const result = await getList();
+    return res.json(result);
+  } catch (error) {
+    console.error('Erro ao listar publicações', error);
+    return res.status(500).json({ error: 'Erro ao buscar publicações' });
+  }
+}
