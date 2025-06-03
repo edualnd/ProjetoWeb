c00db5d8e2790aff566c426864a601bec6d65a21
kevin4bmael <kevin4bmael@gmail.com>
2025-06-03 14:37:59 -0300

    add get, update and delete routes

--- backend/src/routes/post/postRoutes.js
+++ backend/src/routes/post/postRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import createPostController from '../../controllers/post/createPostController.js';
+import updatePostController from '../../controllers/post/updatePostController.js';
+import deletePostController from '../../controllers/post/deletePostController.js';
+import getPostController from '../../controllers/post/getPostController.js';
 
+const postRoutes = express.Router();
 
-const postRoutes = express.Router()
-
-postRoutes.post('/', createPostController)
+postRoutes.get('/list', getPostController);
+postRoutes.post('/', createPostController);
+postRoutes.put('/:authorId/:publicationId', updatePostController);
+postRoutes.delete('/:authorId/:publicationId', deletePostController);
 export default postRoutes;
