38ee0e61f5f0172baefc873315ea616812b5f313
kevin4bmael <kevin4bmael@gmail.com>
2025-06-18 17:23:09 -0300

    add upload photo, video feature

--- backend/src/controllers/post/createPostController.js
+++ backend/src/controllers/post/createPostController.js
@@ -1,10 +1,13 @@
 import { create } from '../../model/postModel.js';
 import postSchema from '../../schemas/postSchema.js';
 import validateSchema from '../../utils/validators/schemaValidator.js';
+import {uploadCloud, uploadVideo} from '../../utils/cloudinary/config.js';
 
 export default async function createPostController(req, res, next) {
 
   try{
+    const photo = req.files;
+    const photoId = [];
     const user = req.user.userId;
     const post = { ...req.body, authorId: user };
     const {text} = req.body;
@@ -13,15 +16,43 @@ export default async function createPostController(req, res, next) {
       text
     });
 
+    let cloudImage
+    if(photo){
+      for(const p of photo)
+       {
+
+        if (p.mimetype.startsWith('image/')){
+          cloudImage = await uploadCloud(p.path);
+        }else if(p.mimetype.startsWith('video/')){
+          cloudImage = await uploadVideo(p.path);
+        }
+
+        if(!cloudImage){
+          return res.status(500).json({ error: error });
+          }
+
+        
+        photoId.push(cloudImage.public_id+ '.' + cloudImage.format)}
+    }
+
     if(!success){
       console.log(error)
       return res.status(500).json({ error: error });
     }
 
-    const result = await create(post);
+    let images = {};
+    
+    if(photo && photoId.length == 2){
+      images = {image: photoId[0],video: photoId[1]}
+    }else if(photo && photoId.length == 1){
+      images = {image: photoId[0]}
+    }
+
+    const result = await create({...post, ...images});
     return res.json({
       message: 'Post criado com sucesso!',
       post: result,
+      
     });
   }catch(error){
     console.log('Erro ao criar o post:', error);
