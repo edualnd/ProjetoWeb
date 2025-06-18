d1161c9568d712d0336f8a31f2b3afe43b8432f8
kevin4bmael <kevin4bmael@gmail.com>
2025-06-18 17:24:24 -0300

    add validation for new values on update

--- backend/src/controllers/post/updatePostController.js
+++ backend/src/controllers/post/updatePostController.js
@@ -1,9 +1,20 @@
 import { update } from '../../model/postModel.js';
+import postSchema from '../../schemas/postSchema.js';
+import validateSchema from '../../utils/validators/schemaValidator.js';
 
 export default async function updatePostController(req, res, next) {
   try {
     const { authorId, publicationId } = req.params;
     const post = req.body;
+
+    const {success, error, data} = await validateSchema(postSchema, {
+      text
+    });
+
+    if(!success){
+      console.log(error)
+      return res.status(500).json({ error: error });
+    }
 
     const result = await update(authorId, +publicationId, post);
 
