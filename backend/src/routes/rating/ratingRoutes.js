98b2bcf486b618e58a79b28c83e3099d905eff76
kevin4bmael <kevin4bmael@gmail.com>
2025-06-18 17:27:42 -0300

    add update and delete controller on router

--- backend/src/routes/rating/ratingRoutes.js
+++ backend/src/routes/rating/ratingRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import createRatingController from '../../controllers/rating/createRatingController.js';
-
+import updateRatingController from '../../controllers/rating/updateRatingController.js';
+import deleteRatingController from '../../controllers/rating/deleteRatingController.js';
 const ratingRoutes = express.Router();
 
 ratingRoutes.post('/:publicationId', createRatingController);
-
+ratingRoutes.put('/:publicationId', updateRatingController);
+ratingRoutes.delete('/:publicationId', deleteRatingController);
 export default ratingRoutes;
