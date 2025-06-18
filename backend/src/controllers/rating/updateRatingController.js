b52f733b14c33993210570b1e93987b06d766532
kevin4bmael <kevin4bmael@gmail.com>
2025-06-18 17:25:37 -0300

    add update rating feature

---
+++ backend/src/controllers/rating/updateRatingController.js
@@ -0,0 +1,31 @@
+import { updateRating } from "../../model/ratingModel.js";
+import validateSchema from '../../utils/validators/schemaValidator.js';
+import ratingSchema from "../../schemas/ratingSchema.js";
+
+
+export default async function updateRatingController(req, res, next){
+    try {
+        const authorId = req.user.userId;
+        const {publicationId} = req.params; 
+        const {rating} = req.body;
+
+        const {success, error, data} = await validateSchema(ratingSchema, {
+        rating
+        });
+
+        if(!success){
+        console.log(error)
+        return res.status(500).json({ error: error });
+        }
+    
+        const result = await updateRating(rating, authorId, +publicationId);
+    
+        return res.json({
+          message: 'Avaliação atualizada com sucesso!',
+          rating: result,
+        });
+      } catch (error) {
+        console.error('Erro ao atualizar avaliação:', error);
+        return res.status(500).json({ error: 'Erro ao atualizar avaliação' });
+    }
+}
