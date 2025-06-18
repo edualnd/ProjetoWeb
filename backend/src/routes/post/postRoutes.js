fb614d9336ac4a7cfbdacdd0afe591b79187fe51
kevin4bmael <kevin4bmael@gmail.com>
2025-06-18 17:26:49 -0300

    add photo, video feature on post route

--- backend/src/routes/post/postRoutes.js
+++ backend/src/routes/post/postRoutes.js
@@ -3,11 +3,12 @@ import createPostController from '../../controllers/post/createPostController.js
 import updatePostController from '../../controllers/post/updatePostController.js';
 import deletePostController from '../../controllers/post/deletePostController.js';
 import getPostController from '../../controllers/post/getPostController.js';
+import upload from '../../utils/multer/config.js';
 
 const postRoutes = express.Router();
 
 postRoutes.get('/list', getPostController);
-postRoutes.post('/', createPostController);
+postRoutes.post('/', upload.array('photos', 2), createPostController);
 postRoutes.put('/:publicationId', updatePostController);
 postRoutes.delete('/:publicationId', deletePostController);
 export default postRoutes;
