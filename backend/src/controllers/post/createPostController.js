1f8dbf973a40eb5a0759f7beb4b938b532f526d7
kevin4bmael <kevin4bmael@gmail.com>
2025-06-17 22:18:32 -0300

    add validation to post creation

--- backend/src/controllers/post/createPostController.js
+++ backend/src/controllers/post/createPostController.js
@@ -1,14 +1,30 @@
 import { create } from '../../model/postModel.js';
+import postSchema from '../../schemas/postSchema.js';
+import validateSchema from '../../utils/validators/schemaValidator.js';
 
 export default async function createPostController(req, res, next) {
-  const user = req.user.userId;
-  console.log(user);
 
-  const post = { ...req.body, authorId: user };
+  try{
+    const user = req.user.userId;
+    const post = { ...req.body, authorId: user };
+    const {text} = req.body;
 
-  const result = await create(post);
-  return res.json({
-    message: 'Post criado com sucesso!',
-    post: result,
-  });
+    const {success, error, data} = await validateSchema(postSchema, {
+      text
+    });
+
+    if(!success){
+      console.log(error)
+      return res.status(500).json({ error: error });
+    }
+
+    const result = await create(post);
+    return res.json({
+      message: 'Post criado com sucesso!',
+      post: result,
+    });
+  }catch(error){
+    console.log('Erro ao criar o post:', error);
+    return res.status(500).json({ error: error.message });
+  }
 }
