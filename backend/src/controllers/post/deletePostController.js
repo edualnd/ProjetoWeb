a5a412999ecec8f1661f1f06cb68b1a370fca03e
kevin4bmael <kevin4bmael@gmail.com>
2025-06-18 17:23:59 -0300

    add delete photo, video from post

--- backend/src/controllers/post/deletePostController.js
+++ backend/src/controllers/post/deletePostController.js
@@ -1,4 +1,5 @@
 import { deletePost } from '../../model/postModel.js';
+import { deleteFromCloud, deleteVideo } from '../../utils/cloudinary/config.js';
 
 export default async function deletePostController(req, res, next) {
   try {
@@ -6,6 +7,34 @@ export default async function deletePostController(req, res, next) {
     const { publicationId } = req.params;
 
     const result = await deletePost(user, +publicationId);
+
+    if(result.image){
+      const idFile = result.image.split('.')
+      let id;
+      if( idFile[0].startsWith('videos/')){
+         id = idFile[0]
+         await deleteVideo(id)
+      }else{
+         id = idFile[0]
+         await deleteFromCloud(id);
+      }
+      
+      console.log(id, idFile, result.video, result.image)
+      await deleteFromCloud(id);
+    }
+    if(result.video){
+      const idFile = result.video.split('.')
+      let id;
+      if( idFile[0].startsWith('videos/')){
+         id = idFile[0]
+         await deleteVideo(id)
+      }else{
+         id = idFile[0]
+         await deleteFromCloud(id);
+      }
+      console.log(id, idFile, result.video, result.image)
+      
+    }
 
     return res.json({
       message: 'Post deletado com sucesso!',
