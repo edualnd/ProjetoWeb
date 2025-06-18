d467d0263f120972253b671f6086e9e582b03c10
kevin4bmael <kevin4bmael@gmail.com>
2025-06-18 17:26:07 -0300

    new features update, delete rating

--- backend/src/model/ratingModel.js
+++ backend/src/model/ratingModel.js
@@ -10,3 +10,27 @@ export async function createRating(rating, authorId, publicationId) {
   return result;
   }
 
+export async function updateRating(rating, authorId, publicationId){
+   const result = await prisma.rating.updateMany({
+    where: {
+      publicationId: Number(publicationId),
+      authorId: authorId
+    },
+    data:  {rating},
+   });
+   return result
+  }
+
+  export async function deleteRating(authorId, publicationId) {
+    const result = await prisma.rating.deleteMany({
+      where: {
+        publicationId: Number(publicationId),
+        authorId: authorId,
+      },
+    });
+    return result;
+  }
+
+export async function getRatingStatics(){
+  
+}
