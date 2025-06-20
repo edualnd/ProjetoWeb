c660eb24aca70ca6ef33eceb262556a2d1099008
kevin4bmael <kevin4bmael@gmail.com>
2025-06-20 14:52:22 -0300

    add average event rating feature

--- backend/src/routes/rating/ratingRoutes.js
+++ backend/src/routes/rating/ratingRoutes.js
@@ -2,8 +2,11 @@ import express from 'express';
 import createRatingController from '../../controllers/rating/createRatingController.js';
 import updateRatingController from '../../controllers/rating/updateRatingController.js';
 import deleteRatingController from '../../controllers/rating/deleteRatingController.js';
+import getRatingStatisticsController from '../../controllers/rating/getRatingStatisticsController.js';
+
 const ratingRoutes = express.Router();
 
+ratingRoutes.get('/:publicationId', getRatingStatisticsController);
 ratingRoutes.post('/:publicationId', createRatingController);
 ratingRoutes.put('/:publicationId', updateRatingController);
 ratingRoutes.delete('/:publicationId', deleteRatingController);
