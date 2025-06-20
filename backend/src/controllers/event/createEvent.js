04157c8240b572c37dca8acdbb8f285ff2af4da6
kevin4bmael <kevin4bmael@gmail.com>
2025-06-20 14:48:44 -0300

    add upload image and video feature in event

--- backend/src/controllers/event/createEvent.js
+++ backend/src/controllers/event/createEvent.js
@@ -1,9 +1,12 @@
 import { createEvent } from '../../model/postModel.js';
 import validateSchema from '../../utils/validators/schemaValidator.js';
 import eventSchema from '../../schemas/eventSchema.js'
+import {uploadCloud, uploadVideo} from '../../utils/cloudinary/config.js';
 
 export default async function createEventController(req, res, next) {
   try {
+    const photo = req.files;
+    const photoId = [];
     const user = req.user.userId;
     const role = req.user.userRole;
     
@@ -14,19 +17,48 @@ export default async function createEventController(req, res, next) {
     const {success, error, data } = await validateSchema(eventSchema, {
       text, title,
     });
+
+    let cloudImage
+    if(photo){
+      for(const p of photo)
+       {
+
+        if (p.mimetype.startsWith('image/')){
+          cloudImage = await uploadCloud(p.path);
+        }else if(p.mimetype.startsWith('video/')){
+          cloudImage = await uploadVideo(p.path);
+        }
+
+        if(!cloudImage){
+          return res.status(500).json({ error: error });
+          }
+
+        
+        photoId.push(cloudImage.public_id+ '.' + cloudImage.format)}
+    }
     
     if(!success){
       console.log(error)
       return res.status(500).json({ error: error });
     }
 
+    let images = {};
+    
+    if(photo && photoId.length == 2){
+      images = {image: photoId[0],video: photoId[1]}
+    }else if(photo && photoId.length == 1){
+      images = {image: photoId[0]}
+    }
+
+
     if (isEvent == true && role != 'COMMOM') {
-      const result = await createEvent(post);
+      const result = await createEvent(...post, ...images);
       return res.json({
         message: 'Evento criado com sucesso!',
         post: result,
       });
     }
+    
     return res.status(200).json({ message: 'voce não e um profissional' });
   } catch (error) {
     console.log('Erro ao criar o evento:', error);
