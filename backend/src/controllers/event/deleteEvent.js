79950cb974f88dcd1883f93811a52c40637b700d
kevin4bmael <kevin4bmael@gmail.com>
2025-06-20 14:49:02 -0300

    add delete image and video feature in event

--- backend/src/controllers/event/deleteEvent.js
+++ backend/src/controllers/event/deleteEvent.js
@@ -1,4 +1,5 @@
 import { deleteEvent } from "../../model/postModel.js";
+import { deleteFromCloud, deleteVideo } from '../../utils/cloudinary/config.js';
 
 export default async function deleteEventController(req, res, next){
   try {
@@ -6,6 +7,34 @@ export default async function deleteEventController(req, res, next){
     const { publicationId } = req.params;
 
     const result = await deleteEvent(user, +publicationId);
+
+      if(result.image){
+      const idFile = result.image.split('.')
+      let id;
+      if( idFile[0].startsWith('videos/')){
+         id = idFile[0]
+         await deleteVideo(id)
+      }else{
+         id = idFile[0]
+         await deleteFromCloud(id);
+      }
+      
+      console.log(id, idFile, result.video, result.image)
+      await deleteFromCloud(id);
+    }
+    if(result.video){
+      const idFile = result.video.split('.')
+      let id;
+      if( idFile[0].startsWith('videos/')){
+         id = idFile[0]
+         await deleteVideo(id)
+      }else{
+         id = idFile[0]
+         await deleteFromCloud(id);
+      }
+      console.log(id, idFile, result.video, result.image)
+      
+    }
 
     return res.json({
       message: 'Evento deletado com sucesso!',
