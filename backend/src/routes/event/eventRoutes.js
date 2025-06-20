5326a786b7305f044cd37aaab95c2a0657f97282
kevin4bmael <kevin4bmael@gmail.com>
2025-06-20 14:51:59 -0300

    add upload to event create route

--- backend/src/routes/event/eventRoutes.js
+++ backend/src/routes/event/eventRoutes.js
@@ -3,11 +3,12 @@ import createEventController from '../../controllers/event/createEvent.js';
 import deleteEventController from '../../controllers/event/deleteEvent.js';
 import getEventController from '../../controllers/event/getEventController.js';
 import updateEventController from '../../controllers/event/updateEventController.js';
+import upload from '../../utils/multer/config.js';
 
 const eventRoutes = express.Router();
 
 eventRoutes.get('/list', getEventController);
-eventRoutes.post('/', createEventController);
+eventRoutes.post('/', upload.array('photos', 2), createEventController);
 eventRoutes.put('/:publicationId', updateEventController);
 eventRoutes.delete('/:publicationId', deleteEventController);
 export default eventRoutes;
