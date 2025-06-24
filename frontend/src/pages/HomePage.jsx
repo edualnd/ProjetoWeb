f11f0fedaf7ae98144ae8b00f9acf5ae7be51edd
Eduarda-Ubuntu <ifeduarda.luiz@gmail.com>
2025-06-24 17:04:41 -0300

    retirando adicoes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,21 +1,7 @@
-import { Button } from "@mui/material";
 import MainContent from "../components/MainContent.jsx";
-
-import { useEffect } from "react";
-
-import { userStore } from "../../store/userStore.js";
-import { postStore } from "../../store/postsStore.js";
 const HomePage = () => {
-  const { loginUser } = userStore();
-
-  const handleClick = async () => {
-    await loginUser({ data: "profi", password: "Duda2006." });
-  };
-
   return (
     <>
-      <Button onClick={handleClick}>CLiquea aqui</Button>
-
       <MainContent></MainContent>
     </>
   );
